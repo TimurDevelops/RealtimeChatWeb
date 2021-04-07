49a74318678e8f80e9ab83d70054fec13aa01d21
TimurDevelops <timur.eremchenko@yandex.ru>
2021-04-08 00:26:52 +0300

    Finished

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,51 @@
 const socket = io();
+const chatForm = document.getElementById('chat-form');
+const chatMessages = document.querySelector('.chat-messages');
+const roomName = document.getElementById('room-name');
+const userList = document.getElementById('users');
+
+const {username, room} = Qs.parse(location.search, {
+    ignoreQueryPrefix: true,
+});
+
+socket.emit('joinRoom', {username, room});
+
+const outputMessage = (message) => {
+    const div = document.createElement('div');
+    div.classList.add('message');
+    div.innerHTML = `<p class="meta">${message.username} <span>${message.time}</span></p>
+        <p class="text">${message.text}</p>`;
+    chatMessages.appendChild(div);
+}
+const outputRoomName = (room) => {
+    roomName.innerText = room;
+}
+const outputUsers = (users) => {
+    userList.innerHTML = `
+        ${users.map(user=>`<li>${user.username}</li>`).join("")}
+    `;
+}
+
+socket.on('roomUsers', ({room, users}) => {
+    outputRoomName(room);
+    outputUsers(users);
+})
+
 
 socket.on('message', message => {
-  console.log(message)
+    outputMessage(message);
+
+    chatMessages.scrollTop = chatMessages.scrollHeight;
 })
+
+// Submit msg
+chatForm.addEventListener('submit', e => {
+    e.preventDefault();
+
+    const msg = e.target.elements.msg.value;
+
+    socket.emit('chatMessage', msg);
+    e.target.elements.msg.value = "";
+    e.target.elements.msg.focus();
+})
+
--- server.js
+++ server.js
@@ -2,6 +2,13 @@ const path = require('path');
 const http = require('http');
 const express = require('express');
 const socketio = require('socket.io');
+const formatMessage = require('./utils/messages');
+const {
+    userJoin,
+    getCurrentUser,
+    getRoomUsers,
+    userLeave
+} = require('./utils/users');
 
 const app = express();
 const server = http.createServer(app)
@@ -11,13 +18,48 @@ const PORT = 3000 || process.env.PORT;
 // Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
 
+const botName = 'ChatCord Bot';
+
 // Run when a client connects
 io.on('connection', socket => {
-  console.log('connetcted')
-  socket.emit('message', 'Welcome to chat')
+    socket.on('joinRoom', ({username, room}) => {
+
+        const user = userJoin(socket.id, username, room)
+
+        socket.join(user.room)
+
+        socket.emit('message', formatMessage(botName, 'Welcome to chat'));
+
+        socket.broadcast.to(user.room).emit('message', formatMessage(botName, `${user.username} has joined the chat`));
+
+        io.to(user.room).emit('roomUsers', {
+            room: user.room,
+            users: getRoomUsers(user.room)
+        })
+
+    })
+
+    socket.on('chatMessage', msg => {
+        const user = getCurrentUser(socket.id)
+        io.emit('message', formatMessage(user.username, msg));
+    })
+
+
+    socket.on('disconnect', () => {
+        const user = userLeave(socket.id);
+        if(user){
+            io.to(user.room).emit('message', formatMessage(botName, `${user.username} has left the chat`));
+            io.to(user.room).emit('roomUsers', {
+                room: user.room,
+                users: getRoomUsers(user.room)
+            })
+        }
+
+    });
 })
 
-server.listen(PORT, ()=>{
-  console.log(`Server running on port ${PORT}`);
+// noinspection JSCheckFunctionSignatures
+server.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
 
